8aec75e786a0f440009ddb0f209730075deae033
odebauche <odebauche@awegroupe.be>
2026-05-31 15:51:27 +0200

    revamp round I

--- includes/functions.php
+++ includes/functions.php
@@ -655,14 +655,87 @@ function glossarySlug(string $term): string
         return '';
     }
 
-    if (!function_exists('iconv')) {
-        $normalized = mb_strtolower($normalized);
-    } else {
-        $normalized = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $normalized);
-        if ($normalized === false) {
-            $normalized = mb_strtolower($term);
-        } else {
-            $normalized = mb_strtolower((string) $normalized);
+    $normalized = strtr($normalized, [
+        "\xC3\xA0" => 'a',
+        "\xC3\xA1" => 'a',
+        "\xC3\xA2" => 'a',
+        "\xC3\xA3" => 'a',
+        "\xC3\xA4" => 'a',
+        "\xC3\xA5" => 'a',
+        "\xC3\xA7" => 'c',
+        "\xC3\xA8" => 'e',
+        "\xC3\xA9" => 'e',
+        "\xC3\xAA" => 'e',
+        "\xC3\xAB" => 'e',
+        "\xC3\xAC" => 'i',
+        "\xC3\xAD" => 'i',
+        "\xC3\xAE" => 'i',
+        "\xC3\xAF" => 'i',
+        "\xC3\xB1" => 'n',
+        "\xC3\xB2" => 'o',
+        "\xC3\xB3" => 'o',
+        "\xC3\xB4" => 'o',
+        "\xC3\xB5" => 'o',
+        "\xC3\xB6" => 'o',
+        "\xC3\xB8" => 'o',
+        "\xC3\xB9" => 'u',
+        "\xC3\xBA" => 'u',
+        "\xC3\xBB" => 'u',
+        "\xC3\xBC" => 'u',
+        "\xC3\xBD" => 'y',
+        "\xC3\xBF" => 'y',
+        "\xC3\x80" => 'a',
+        "\xC3\x81" => 'a',
+        "\xC3\x82" => 'a',
+        "\xC3\x83" => 'a',
+        "\xC3\x84" => 'a',
+        "\xC3\x85" => 'a',
+        "\xC3\x86" => 'ae',
+        "\xC3\x87" => 'c',
+        "\xC3\x88" => 'e',
+        "\xC3\x89" => 'e',
+        "\xC3\x8A" => 'e',
+        "\xC3\x8B" => 'e',
+        "\xC3\x8C" => 'i',
+        "\xC3\x8D" => 'i',
+        "\xC3\x8E" => 'i',
+        "\xC3\x8F" => 'i',
+        "\xC3\x90" => 'd',
+        "\xC3\x91" => 'n',
+        "\xC3\x92" => 'o',
+        "\xC3\x93" => 'o',
+        "\xC3\x94" => 'o',
+        "\xC3\x95" => 'o',
+        "\xC3\x96" => 'o',
+        "\xC3\x98" => 'o',
+        "\xC3\x99" => 'u',
+        "\xC3\x9A" => 'u',
+        "\xC3\x9B" => 'u',
+        "\xC3\x9C" => 'u',
+        "\xC3\x9F" => 'ss',
+        "\xC5\x92" => 'oe',
+        "\xC5\x93" => 'oe',
+        "\xE2\x80\x98" => ' ',
+        "\xE2\x80\x99" => ' ',
+        "\xE2\x80\x9C" => ' ',
+        "\xE2\x80\x9D" => ' ',
+        "\xC2\xAB" => ' ',
+        "\xC2\xBB" => ' ',
+        "\xE2\x80\x93" => ' ',
+        "\xE2\x80\x94" => ' ',
+        "\xE2\x80\xA6" => ' ',
+        "\xC2\xA0" => ' ',
+        "\xC3\x86" => 'ae',
+        "\xC3\x9E" => 'p',
+    ]);
+
+    $normalized = mb_strtolower($normalized, 'UTF-8');
+    $normalized = preg_replace('/[\'"`]/', ' ', $normalized);
+
+    if (function_exists('iconv')) {
+        $converted = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $normalized);
+        if (is_string($converted) && $converted !== '') {
+            $normalized = $converted;
         }
     }
 
@@ -676,7 +749,6 @@ function glossarySlug(string $term): string
 }
 
 /**
- * @param array<int, array<string, mixed>> $glossary
  * @return array<string, mixed>|null
  */
 function glossaryTermBySlug(array $glossary, string $slug): ?array
@@ -845,3 +917,4 @@ function siteBaseUrl(): string
 
     return 'https://myagri.be';
 }
+
